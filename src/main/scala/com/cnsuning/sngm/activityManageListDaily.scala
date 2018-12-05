5999c93b3755db892ab0155b46c1442fb586864a
18040267 <ericpan24@gmail.com>
2018-12-05 18:11:40 +0800

    commit from company

--- src/main/scala/com/cnsuning/sngm/activityManageListDaily.scala
+++ src/main/scala/com/cnsuning/sngm/activityManageListDaily.scala
@@ -1,9 +1,23 @@
 package com.cnsuning.sngm
 
-/**Program used to deal with Activity Management List module(AML)
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SparkSession
+import org.slf4j.LoggerFactory
+
+/**Program used to deal with Activity Management List module(AML), on daily frequency
   *
   *
   */
-object activityManageListDaily {
 
+
+object activityManageListDaily {
+  def main(args: Array[String]): Unit = {
+    val logger = LoggerFactory.getLogger(activityManageListDaily.getClass)
+
+    val sc = new SparkConf().setAppName("actMngLstDlyExector").setMaster("local")
+//    val sparkSession = SparkSession.builder().config(sc).enableHiveSupport().getOrCreate()
+    val sparkContext = new SparkContext(sc)
+    val rdd = sparkContext.parallelize(args.toSeq)
+    logger.info("==============Args_output===============" + ":{}",rdd.collect())
+  }
 }
