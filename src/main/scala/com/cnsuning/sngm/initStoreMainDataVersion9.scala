d15ec0a700ba9e1feb95e3d9d4686145f9b1d445
18040267 <ericpan24@gmail.com>
2018-12-19 11:04:23 +0800

    commit from working pc

--- src/main/scala/com/cnsuning/sngm/initStoreMainDataVersion9.scala
+++ src/main/scala/com/cnsuning/sngm/initStoreMainDataVersion9.scala
@@ -37,30 +37,33 @@ object initStoreMainDataVersion9 {
       .set("spark.sql.hive.metastorePartitionPruning", "false")
 
     //    define value for program
-    val querySqlStr="select str_cd,str_nm,cmpy_cd,cmpy_nm,area_cd,city_cd,city_nm,str_addr,str_busi_fmt_cd,str_status,site_tp_cd,vrtl_flg from brock_dim.t_spl_str_tree_ed a"
+    val querySqlStr="select str_cd,str_nm,cmpy_cd,cmpy_nm,area_cd,city_cd,city_nm,str_addr,str_busi_fmt_cd,str_status,site_tp_cd,vrtl_flg,str_prop_cd from brock_dim.t_spl_str_tree_ed a"
 //    val querySqlCity = "select admin_city_cd,city_cd from bi_sor.tsor_org_city_conv_rel_td t  "
-//    val querySqlStrType = "select idry_site_tp_cd,sub_plant_tp_cd,str_type from sospdm.store_type_logic t"
+    val querySqlStrType = "select idry_site_tp_cd,sub_plant_tp_cd,str_type from sospdm.store_type_logic t"
     val session = SparkSession.builder().config(sc).enableHiveSupport().getOrCreate()
 
     //    get store information with admin city_cd
     val str = session.sql(querySqlStr)
     val strDFin = str
-      .drop("city_cd")
       .where("str_status = 1 and site_tp_cd = 2 and vrtl_flg = 0 ")
       .drop("str_status").drop("site_tp_cd").drop("vrtl_flg")
       .filter(row => {val str_nm = row.getAs[String]("str_nm")
         val str_addr = row.getAs[String]("str_addr")
         str_nm != "待定" && str_nm != "待用" && str_addr != "待定" && str_addr != "待用" && !str_nm.contains("测试")
       })
+      .filter(!col("city_cd").like("HK%"))
+//      .filter(!col("str_addr")==="-")
+      .filter(!col("str_nm").like("%虚拟%"))
       .withColumnRenamed("cmpy_cd","org_cd")
       .withColumnRenamed("str_addr","addr")
-      .withColumnRenamed("str_busi_fmt_cd","str_type")
+      .withColumnRenamed("str_busi_fmt_cd","idry_site_tp_cd")
+      .withColumnRenamed("str_prop_cd","sub_plant_tp_cd")
       .withColumn("longitude",lit(0))
       .withColumn("latitude",lit(0))
       .withColumn("district_nm",lit(""))
 
     //  change store name to be a real store name when it contain its' city name within a UDF Function
-    val rsltDF = clearStoreName(strDFin,"str_nm","city_nm")
+    val strDF = clearStoreName(strDFin,"str_nm","city_nm")
 
     //    get city information to replace the admin city_cd
 //    val cityDF = session.sql(querySqlCity)
@@ -69,7 +72,7 @@ object initStoreMainDataVersion9 {
 //      .distinct()
 
     //    get store type information to be joined
-//    val strType = session.sql(querySqlStrType).distinct()
+    val strType = session.sql(querySqlStrType).distinct()
 
     //    inner join the store information to replace a admin_city_cd became a city_cd: 1000017 -> 025
 //    val DF = strDF
@@ -85,8 +88,8 @@ object initStoreMainDataVersion9 {
 //      .drop("admin_city_cd")
     //      .drop("org_cd")
 
-//    val rsltDF = str.join(strType,Seq("idry_site_tp_cd","sub_plant_tp_cd"),"inner")
-//      .drop("idry_site_tp_cd").drop("sub_plant_tp_cd")
+    val rsltDF = strDF.join(strType,Seq("idry_site_tp_cd","sub_plant_tp_cd"),"inner")
+      .drop("idry_site_tp_cd").drop("sub_plant_tp_cd")
 
     rsltDF.write.mode("overwrite").saveAsTable("sospdm.t_sngm_init_str_original") // 此句会Drop原先的表，然后按照DF的内容自己建立一个。
 
